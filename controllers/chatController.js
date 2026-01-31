58a72def2c0a968768854027318d3138d5dee71d
goel-stu-amrit <amritgoel2206@gmail.com>
2026-01-31 13:08:49 +0530

    Added read/unread Message count functionality

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const authMiddleware = require('../middlewares/authMiddleware')
 const Chat = require('./../models/chat')
-
+const Message = require('../models/message')
 
 router.post('/create-new-chat', authMiddleware, async (req,res)=>{
     try{
@@ -26,6 +26,9 @@ router.post('/create-new-chat', authMiddleware, async (req,res)=>{
 router.get('/get-all-chats', authMiddleware, async (req,res)=>{
     try{
         const allChats = await Chat.find({members:{$in : req.userId}})
+                                .populate('members')
+                                .populate('lastMessage')
+                                .sort({updatesAt :-1})
 
         res.send({
             message:"chats fetched successfully",
@@ -36,6 +39,42 @@ router.get('/get-all-chats', authMiddleware, async (req,res)=>{
         res.status(400).send({
             message: err.message,
             success:false
+        })
+    }
+})
+
+router.post('/clear-unread-message', authMiddleware, async (req, res)=>{
+    try{
+        const chatId = req.body.chatId
+
+        //We want to update he unread message count in chat collection
+        const chat = await Chat.findById(chatId)
+        if(!chat){
+            res.send({
+                message:"No Chat Found",
+                success: false,
+            })
+        }
+        const updatedChat = await Chat.findByIdAndUpdate(chatId,
+            { unreadMessageCount:0},
+            { new:true}
+        ).populate('members').populate('lastMessage')
+
+        //we want to update the read property to true in message collection
+        await Message.updateMany(
+            { chatId: chatId, read: false},
+            { read: true}
+        )
+        res.send({
+            message: "Unread message cleared successfully",
+            success: true,
+            data: updatedChat
+        })
+    }catch(error){
+        res.send({
+            message:error,
+            success: false,
+
         })
     }
 })
