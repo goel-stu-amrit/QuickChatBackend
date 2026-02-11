666cc211155eec8e195226a5f70db48eee06288d
goel-stu-amrit <amritgoel2206@gmail.com>
2026-02-11 17:10:10 +0530

    added cors ready to deploy

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const authRouter = require('./controllers/authController')
 const userRouter = require('./controllers/userController')
 const chatRouter = require('./controllers/chatController')
 const messageRouter = require('./controllers/messageController')
 
+app.use(cors())
 app.use(express.json({
     limit:'50mb'
 }))
